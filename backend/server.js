84003772aa75eb322eda19d6285fc8590b2ee749
srikanth <srikanth.jeeva1997@gmail.com>
2026-05-25 17:47:03 +0530

    fix: port name spelling fix

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
